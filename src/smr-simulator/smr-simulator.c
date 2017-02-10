07d752be3660c730f472aaccd3932973ed16e431
AlexNewTao <taoliu012@126.com>
2017-02-10 19:01:41 +0800

    GetSMROffset and GetSMRBandNum

--- src/smr-simulator/smr-simulator.c
+++ src/smr-simulator/smr-simulator.c
@@ -106,14 +106,18 @@ static void* freeStrategySSD()
             //分配锁
             pthread_mutex_lock(&free_ssd_mutex);
 			interval_time = 0;
+			//遍历整个ssd内部，如果ssd_flag标记为可以数据，flush 数据
 			for (i = ssd_strategy_control->first_usedssd; i < ssd_strategy_control->first_usedssd + NSSDCLEAN; i++) {
 				if (ssd_descriptors[i%NSSDs].ssd_flag & SSD_VALID) {
 					flushSSD(&ssd_descriptors[i%NSSDs]);
 				}
 			}
+			//修改位置
 			ssd_strategy_control->first_usedssd = (ssd_strategy_control->first_usedssd + NSSDCLEAN) % NSSDs;
+
+			//已经使用的数据需要减去clean的数据量
 			ssd_strategy_control->n_usedssd -= NSSDCLEAN;
-			//releaselock
+			//处理完成后releaselock
             pthread_mutex_unlock(&free_ssd_mutex);
             if (DEBUG) 
                 printf("[INFO] freeStrategySSD():--------after clean\n");
@@ -121,20 +125,38 @@ static void* freeStrategySSD()
 	}
 }
 
+//flush函数，把ssd中的数据flush到底层存储；
+/*
+一个定义为volatile的变量是说这变量可能会被意想不到地改变，
+这样，编译器就不会去假设这个变量的值了。精确地说就是，优
+化器在用到这个变量时必须每次都小心地重新读取这个变量的值，
+而不是使用保存在寄存器里的备份;
 
+在这里是修饰函数的返回值
+这里的作用是帮助编译器进行优化；准确来说就是
+但是加了volatile过后，就意味着这个函数不会返回，就相当于
+告诉编译器，我调用后是不用保存调用我的函数的返回地址的。
+这样就达到了优化的作用。
+*/
 static volatile void* flushSSD(SSDDesc *ssd_hdr)
 {
 	long i;
     int returnCode;
+    //buffer的大小为4k
 	char buffer[BLCKSZ];
+	//BNDSZ的大小为8k，char* band[2];
 	char* band[BNDSZ/BLCKSZ];
+	//定义bool数组；判断使用的数据
 	bool bandused[BNDSZ/BLCKSZ];
+
 	unsigned long BandNum = GetSMRBandNumFromSSD(ssd_hdr);
 	off_t Offset;
 
 	memset(bandused, 0, BNDSZ/BLCKSZ*sizeof(bool));
 	for (i = 0; i < BNDSZ/BLCKSZ; i++)
 		band[i] = (char *) malloc(sizeof(char)*BLCKSZ);
+
+	//pread是一个函数，用于带偏移量地原子的从文件中读取数据。
 	returnCode = pread(inner_ssd_fd, band[GetSMROffsetInBandFromSSD(ssd_hdr)], BLCKSZ, ssd_hdr->ssd_id * BLCKSZ);
 	if(returnCode < 0) {
 		printf("[ERROR] flushSSD():-------read from inner ssd: fd=%d, errorcode=%d, offset=%lu\n", inner_ssd_fd, returnCode, ssd_hdr->ssd_id * BLCKSZ);
@@ -176,4 +198,14 @@ static volatile void* flushSSD(SSDDesc *ssd_hdr)
 	}
 }
 
+//从SSD得到SMR的band数目；用偏移量/band的大小;
+static unsigned long GetSMRBandNumFromSSD(SSDDesc *ssd_hdr)
+{
+	return ssd_hdr->ssd_tag.offset / BNDSZ;
+}
 
+
+static off_t GetSMROffsetInBandFromSSD(SSDDesc *ssd_hdr)
+{
+	return (ssd_hdr->ssd_tag.offset / BLCKSZ) % (BNDSZ / BLCKSZ);
+}
