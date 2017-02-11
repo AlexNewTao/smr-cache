ad0fdadbdd791e50adff4db72672babce5d6032e
AlexNewTao <taoliu012@126.com>
2017-02-11 13:59:18 +0800

    add information in ssd buffer table

--- src/ssd_buff_table.c
+++ src/ssd_buff_table.c
@@ -9,9 +9,11 @@
 
 void initSSDBufTable(size_t size)
 {
+	//分配空间
 	ssd_buffer_hashtable = (SSDBufferHashBucket *)malloc(sizeof(SSDBufferHashBucket)*size);
 	size_t i;
 	SSDBufferHashBucket *ssd_buf_hash = ssd_buffer_hashtable;
+	//参数初始化
 	for (i = 0; i < size; ssd_buf_hash++, i++){
 		ssd_buf_hash->ssd_buf_id = -1;
 		ssd_buf_hash->hash_key.offset = -1;
--- src/ssd_buff_table.h
+++ src/ssd_buff_table.h
@@ -1,6 +1,17 @@
 #ifndef SSDBUFTABLE_H
 #define SSDBUFTABLE_H
 
+typedef struct SSDBufferHashBucket
+{
+	SSDBufferTag 			hash_key;
+	long    				ssd_buf_id;
+	struct SSDBufferHashBucket 	*next_item;
+} SSDBufferHashBucket;
+
+
+
+
+
 extern void initSSDBufTable(size_t size);
 
 
